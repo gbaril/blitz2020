9d4180c297829ff9306578bf776a5f67dc2a1dac
Guillaume Baril <barilgui@gmail.com>
2020-01-18 11:16:13 -0500

    a

--- utils.py
+++ utils.py
@@ -10,30 +10,35 @@ def min_dist_enemy_to_tail(game_message: GameMessage):
     min_dist = 1000000
 
     me = get_me(game_message)
-    grille = [[game_message.game.map[i][j] for j in range(len(game_message.game.map[0]))]for i in range(len(game_message.game.map))]
+    grille = game_message.game.map
     
     for case in me.tail:
-        grille[case.y][case.x] = TileType.TAIL
+        grille[case.y][case.x] = TileType.TAIL.value
 
     for player in game_message.players:
         if player.id != me.id:
-            path = bfs(grille, player.position, [TileType.ASTEROIDS, TileType.BLACK_HOLE], [TileType.TAIL])
+            print(grille)
+            path = bfs(grille, player.position, [TileType.ASTEROIDS.value, TileType.BLACK_HOLE.value], [TileType.TAIL.value])
             dist = len(path)
             #print(path)
             if dist < min_dist:
                 min_dist = dist
 
+    print('fin enemy')
     return min_dist
 
 def min_dist_us_to_base(game_message: GameMessage):
     me = get_me(game_message)
-    grille = [[game_message.game.map[i][j] for j in range(len(game_message.game.map[0]))]for i in range(len(game_message.game.map))]
+    grille = game_message.game.map
 
     for case in me.tail:
-        grille[case.y][case.x] = TileType.TAIL
+        grille[case.y][case.x] = TileType.TAIL.value
 
     end_tail = me.tail[0]
-    grille[end_tail.y][end_tail.x] = TileType.END_TAIL
+    grille[end_tail.y][end_tail.x] = TileType.END_TAIL.value
 
-    path = bfs(grille, me.position, [TileType.ASTEROIDS, TileType.BLACK_HOLE, TileType.TAIL], [TileType.END_TAIL, str(TileType.CONQUERED) + str(me.id)])
+    print(grille)
+    path = bfs(grille, me.position, [TileType.ASTEROIDS.value, TileType.BLACK_HOLE.value, TileType.TAIL.value], [TileType.END_TAIL.value, TileType.CONQUERED.value + str(me.id)])
+
+    print('fin us')
     return len(path)
