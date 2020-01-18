d89299e1e1c0e778fa84f399024cb711dc6b37f0
Adam Jimenez <jimenez.adam0@gmail.com>
2020-01-18 09:23:18 -0500

    bfs (belles fesses)

---
+++ ai_utils/bfs.py
@@ -0,0 +1,59 @@
+import collections
+
+grille = [
+    [0,0,0,0,0],
+    [0,0,1,0,0],
+    [2,0,1,0,3],
+    [0,0,1,0,0],
+    [0,0,0,0,0]
+]
+
+# point: (i,j)
+def neighbors(grille, point):
+    i,j = point
+    neighbors = []
+    if i > 0:
+        neighbors.append((i-1, j))
+    if j > 0:
+        neighbors.append((i, j-1))
+    if i < len(grille) - 1:
+        neighbors.append((i+1, j))
+    if j < len(grille[0]) - 1:
+        neighbors.append((i, j+1))
+    return neighbors
+
+# previous: dict[point] = point, end = point
+def backtrack(previous, end):
+    points = []
+    points.insert(0, end)
+    prev = end
+    while prev != previous[prev]:
+        prev = previous[prev]
+        points.insert(0, prev)
+
+    return points
+
+# root: (i,j) obstacles: [1] target: [3]
+def bfs(grille, root, obstacles, target):
+    seen, previous, queue = set(), dict(), collections.deque([root])
+    previous[root] = root
+    while queue:
+        point = queue.popleft()
+        seen.add(point)
+
+        pi, pj = point
+        if grille[pi][pj] in target:
+            return backtrack(previous, point)
+
+        for neighbor in neighbors(grille, point):
+            if neighbor in seen:
+                continue
+            previous[neighbor] = point
+            ni, nj = neighbor
+            if grille[ni][nj] in obstacles:
+                continue
+            else:
+                queue.append(neighbor)
+
+if __name__ == "__main__":
+    print(bfs(grille, (2,0), obstacles=[1], target=[3]))
